032471266d0e218a8c6216e47616c8ec3d62c856
sherry wen <rikka9816@gmail.com>
2023-11-16 18:21:00 +1300

    mod: configure tailwind / refine layout
    configured tailwind, refine layout with element-plus layout

--- resources/js/components/BlogHeader.vue
+++ resources/js/components/BlogHeader.vue
@@ -1,44 +1,23 @@
 <script>
+import { Search, Minus } from '@element-plus/icons-vue'
 export default {
     name: "BlogHeader",
+    components:{
+        Search,
+        Minus
+    },
     data(){
         return{
-            activeIndex: "1"
-        }
-    },
-    methods:{
-        handleSelect(key,keyPath){
-            console.log(key, keyPath)
+            input: ''
         }
     }
 }
 </script>
 
 <template>
-    <el-menu
-        :default-active="activeIndex"
-        class="el-menu-demo"
-        mode="horizontal"
-        @select="handleSelect"
-    >
-        <el-menu-item index="1">Processing Center</el-menu-item>
-        <el-sub-menu index="2">
-            <template #title>Workspace</template>
-            <el-menu-item index="2-1">item one</el-menu-item>
-            <el-menu-item index="2-2">item two</el-menu-item>
-            <el-menu-item index="2-3">item three</el-menu-item>
-            <el-sub-menu index="2-4">
-                <template #title>item four</template>
-                <el-menu-item index="2-4-1">item one</el-menu-item>
-                <el-menu-item index="2-4-2">item two</el-menu-item>
-                <el-menu-item index="2-4-3">item three</el-menu-item>
-            </el-sub-menu>
-        </el-sub-menu>
-        <el-menu-item index="3" disabled>Info</el-menu-item>
-        <el-menu-item index="4">Orders</el-menu-item>
-    </el-menu>
+   <div class="h-full bg-black text-white">header</div>
 </template>
 
-<style scoped>
+<style scoped lang="less">
 
 </style>
