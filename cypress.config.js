cf7e9feed01a18d16873c91df3cb455f55aefcbb
sherry wen <rikka9816@gmail.com>
2023-12-25 11:28:26 +1300

    mod: reconfigure cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,27 +1,9 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  chromeWebSecurity: false,
-  retries: 2,
-  defaultCommandTimeout: 5000,
-  watchForFileChanges: false,
-  videosFolder: "tests/cypress/videos",
-  screenshotsFolder: "tests/cypress/screenshots",
-  fixturesFolder: "tests/cypress/fixture",
-
   e2e: {
     setupNodeEvents(on, config) {
-      return require("./tests/cypress/plugins/index.js")(on, config);
-    },
-    baseUrl: "http://laravel_vue_blog.test",
-    specPattern: "tests/cypress/integration/**/*.cy.{js,jsx,ts,tsx}",
-    supportFile: "tests/cypress/support/index.js",
-  },
-
-  component: {
-    devServer: {
-      framework: "vue",
-      bundler: "vite",
+      // implement node event listeners here
     },
   },
 });
